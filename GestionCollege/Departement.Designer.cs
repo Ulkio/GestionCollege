1bd30c0b544e252842843fbfa97c125d08797627
guillaume <guillaume.verschave@gmail.com>
2018-04-18 09:06:16 +0200

    j

--- GestionCollege/Departement.Designer.cs
+++ GestionCollege/Departement.Designer.cs
@@ -43,7 +43,6 @@
             this.lstEnseignants = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lstDepartement = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtMoyenne
@@ -179,21 +178,11 @@
             this.lstDepartement.Size = new System.Drawing.Size(176, 147);
             this.lstDepartement.TabIndex = 30;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(690, 319);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 45;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Departement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(777, 354);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtMoyenne);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtResponsable);
@@ -233,6 +222,5 @@
         private System.Windows.Forms.ListBox lstEnseignants;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox lstDepartement;
-        private System.Windows.Forms.Button button1;
     }
 }
